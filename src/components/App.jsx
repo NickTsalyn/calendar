0f0d8859378e05207d0153a332a1f240487ea1ea
NickTsalyn <nicktsalyn@gmail.com>
2023-09-11 18:08:30 -0400

    create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import data from '../data.json';
+import { List, ListItem } from './App.styled';
+import { Wrapper } from './Layout.styled';
+
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrapper>
+      <List>
+        {data.map(date => {
+           const backgroundColor = getRandomColor();
+          return (
+            <ListItem key={date.id} style={{ background: backgroundColor }}>
+              <p>{date.id} Day</p>
+              <p>{date.bank}</p>
+              <p>{date.payment}$</p>
+            </ListItem>
+          );
+        })}
+      </List>
+    </Wrapper>
   );
 };
---
+++ src/components/App.styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+
+export const List = styled.ul`
+display: flex;
+flex-wrap: wrap;
+gap: 10px;
+list-style: none;
+align-items: center;
+`
+
+export const ListItem = styled.li`
+display: flex;
+flex-direction: column;
+gap: 10px;
+border: solid 2px black;
+width: 200px;
+height: 110px;
+justify-content: center;
+align-items: center;
+font-weight: 700;
+font-size: 20px;`
